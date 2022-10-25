868f93b858f5b2c5b65225591e62b4f690f986d3
Chenyang Zhong <zhongcy95@gmail.com>
2022-10-25 09:57:04 +0000

    sm8150-common: rootdir: Copy fstab to ramdisk as well
    Needed by dynamic partitioning [1].
    
    [1] https://source.android.com/devices/tech/ota/dynamic_partitions/implement#fstab-changes
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- common.mk
+++ common.mk
@@ -240,6 +240,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.lenovo_msmnile
 
+# Fstab
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
